4244aa49c3559ceaf45d86c0b2054db6a69066e5
juhee0614 <85140495+juhee0614@users.noreply.github.com>
2024-04-29 20:46:33 +0900

    feat : init

---
+++ src/main/java/com/project/whoareyou/WhoareyouApplication.java
@@ -0,0 +1,13 @@
+package com.project.whoareyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoareyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoareyouApplication.class, args);
+	}
+
+}
